36c428ef2140a7edb558e23dc423c3134d4e2a0a
MrT-Branham <sturner@cuhsd.org>
2024-10-15 14:49:14 -0700

    .gitignore file

--- src/Tester.java
+++ src/Tester.java
@@ -2,5 +2,6 @@ public class Tester {
     public static void main(String[] args) {
         Dog rob = new Dog("Corgi", "Rob", 5);
         System.out.println(rob);
+        System.out.println("\"To be or not to be that is the question\" - Shakespeare");
     }
 }
