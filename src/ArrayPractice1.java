55a870ccc5f2fbad729bf86145c0aec7fff04c3d
MrT-Branham <sturner@cuhsd.org>
2025-01-08 18:13:59 -0800

    Added Array Practice

---
+++ src/ArrayPractice1.java
@@ -0,0 +1,10 @@
+/**
+ * Prints every element from args.
+ * args: 5 9 125 -64 78 -1 359
+ * @author Mr. Turner
+ */
+public class ArrayPractice1 {
+    public static void main(String[] args){
+        // TODO 1: Print every element in the array with a enhanced for loop.
+    }
+}
