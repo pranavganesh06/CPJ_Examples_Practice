7ee8cb56198c9f7ab378af04eecc161f9fae1a52
MrT-Branham <sturner@cuhsd.org>
2025-03-11 13:51:27 -0700

    Added solution to Table.java

--- src/Table.java
+++ src/Table.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 public class Table extends Furniture{
     private int legs;
     //Constructor with no arguments
+    public Table(){
+        super();
+        this.legs = 4;
+    }
 
     //Overloaded constructor with arguments for materials and legs
+    public Table(ArrayList<String> materials, int legs){
+        super(materials);
+        this.legs = legs;
+    }
 
     public void setLegs(int legs){this.legs = legs; }
     public int getLegs(){return legs;}
