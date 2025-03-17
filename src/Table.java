2dce6612f1ef56a75c41ea69d2744135f06bc5ee
MrT-Branham <sturner@cuhsd.org>
2025-03-17 13:52:04 -0700

    Added toString and equals to Table

--- src/Table.java
+++ src/Table.java
@@ -16,4 +16,34 @@ public class Table extends Furniture{
 
     public void setLegs(int legs){this.legs = legs; }
     public int getLegs(){return legs;}
+
+    public String toString(){
+        StringBuilder output = new StringBuilder(String.format(
+                "%s[legs=%d, materials=",
+                getClass().toString(),
+                legs));
+        if (getMaterials().size() == 0){
+            output.append("none]");
+        }
+        else{
+            for(String material: getMaterials()){
+                output.append(material + ",");
+            }
+            output.deleteCharAt(output.length() -1);
+            output.append("]");
+        }
+        return output.toString();
+    }
+
+    public boolean equals(Object otherObject){
+        if (otherObject == null){
+            return false;
+        }
+        if (getClass() != otherObject.getClass()){
+            return false;
+        }
+        Table other = (Table) otherObject;
+        return legs == other.legs &&
+                getMaterials().size() == other.getMaterials().size() &&
+                getMaterials().containsAll(other.getMaterials()); }
 }
