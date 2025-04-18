af872c99a00f4b7fe202298779327dea7cf41563
MrT-Branham <sturner@cuhsd.org>
2025-04-18 09:20:25 -0700

    Added CharSequenceDemo solved

--- src/CharSequenceDemo.java
+++ src/CharSequenceDemo.java
@@ -31,7 +31,54 @@
 
 // CharSequenceDemo presents a String value -- backwards.
 public class CharSequenceDemo implements CharSequence {
-    //TODO Finish this class.
+    private String s;
+
+    public CharSequenceDemo(String s) {
+        //It would be much more efficient to just reverse the string
+        //in the constructor. But a lot less fun!
+        this.s = s;
+    }
+
+    //If the string is backwards, the end is the beginning!
+    private int fromEnd(int i) {
+        return s.length() - 1 - i;
+    }
+
+    public char charAt(int i) {
+        if ((i < 0) || (i >= s.length())) {
+            throw new StringIndexOutOfBoundsException(i);
+        }
+        return s.charAt(fromEnd(i));
+    }
+
+    public int length() {
+        return s.length();
+    }
+
+    public CharSequence subSequence(int start, int end) {
+        if (start < 0) {
+            throw new StringIndexOutOfBoundsException(start);
+        }
+        if (end > s.length()) {
+            throw new StringIndexOutOfBoundsException(end);
+        }
+        if (start > end) {
+            throw new StringIndexOutOfBoundsException(start - end);
+        }
+        StringBuilder sub =
+                new StringBuilder(s.subSequence(fromEnd(end), fromEnd(start)));
+        return sub.reverse();
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder(this.s);
+        return s.reverse().toString();
+    }
+
+    //Random int from 0 to max. As random() generates values between 0 and 0.9999
+    private static int random(int max) {
+        return (int) Math.round(Math.random() * (max+1));
+    }
 
     public static void main(String[] args) {
         CharSequenceDemo s =
