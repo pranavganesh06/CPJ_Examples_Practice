698b01bc3fca71ca97e212d5ebaa03e1d68ab84a
sturner <sturner@cuhsd.org>
2025-09-18 10:48:20 -0700

    Updated Dog and wrote DogTester

--- src/Dog.java
+++ src/Dog.java
@@ -2,8 +2,14 @@
  * Represents a basic dog that can fetch and speak.
  */
 public class Dog {
-    private String speech = "Woof!";
-    private String name = "Bark Choy";
+    public static final String GENUS = "Canis";
+    private String speech;
+    private String name;
+
+    public Dog(String dogName, String dogSpeech){
+        speech = dogSpeech;
+        name = dogName;
+    }
 
     /**
      * Dog retrieves specified object.
